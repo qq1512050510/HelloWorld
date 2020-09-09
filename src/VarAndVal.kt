0510049f5220ed7a37173803e32edc1b429fe852
jiangyupeng <765819328@qq.com>
2020-09-09 11:18:23 +0800

    kotlin Learn

---
+++ src/VarAndVal.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>) {
+	var name:String = "张三"
+	name = "李四"
+	println(name)
+	
+	//kotlin 智能类型推断
+	var a = 123
+	//常量
+	val m = "constant"
+	var aByte:Byte = Byte.MAX_VALUE;
+	var bByte:Byte = Byte.MIN_VALUE
+	println(aByte)
+	println(bByte)
+}
